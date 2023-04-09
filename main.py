b24e4ff7b18e8543f84b89f4c2ed8107d9178791
CiZ01 <ffrancy01@gmail.com>
2023-04-09 22:50:20 +0200

    added context class created lang file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
+import json
+import logging
+import sys
+
+sys.path.append('service/api')
+import contextApi as context
 
 
 # ------------------ INIT ------------------ #
@@ -8,12 +15,36 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+ctx = context.ContextApi()
+
+# ------------------  ------------------ #
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda message: True)
+    # load language
+    ctx.user_lang = message.from_user.language_code
+    if not os.path.exists(f'service/api/utils_files/languages/{ctx.user_lang}_texts.json'):
+        ctx.user_lang = 'en'
+    with open(f'service/api/utils_files/languages/{ctx.user_lang}_texts.json', 'r') as json_file:
+        txt = json.load(json_file)
+        ctx.msg_txt = txt["messages"]
+        ctx.btn_txt = txt["buttons"]
+
+    # send welcome message
+    bot.reply_to(message, ctx.msg_txt["start_message"])
+
+
+@bot.message_handler(func=lambda message: True and message.text[0] == 'c')
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
+
+logger.info("**Bot started**")
 bot.infinity_polling()
---
+++ service/api/contextApi.py
@@ -0,0 +1,24 @@
+class ContextApi():
+    msg_text = {}
+    btn_text = {}
+
+    userid = None
+    user_lang = None
+
+    def __init__(self):
+        return
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __delitem__(self, key):
+        setattr(self, key, None)
+
+    def __contains__(self, key):
+        return hasattr(self, key)
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
